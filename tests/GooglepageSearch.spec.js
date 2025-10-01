ea01ad222c3492c9c9a4f2ceecca90dc75495a04
Emariele0408 <emariele0408@icloud.com>
2025-10-01 16:44:33 -0300

    main

--- tests/GooglepageSearch.spec.js
+++
@@ -1,7 +0,0 @@
-import { test } from "@playwright/test"
-
-test("Google page Search", async ({ page }) => {
-    await page.goto("https://www.google.com");
-    await page.getByRole('combobox', { name: 'Pesquisar' }).fill("teste");
-    await page.getByRole('button', { name: 'Pesquisa Google' }).first().click();
-})
